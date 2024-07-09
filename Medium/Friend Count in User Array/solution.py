41be49492e1328e2cb090c0d02ba1d443292cd6c
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-09 10:56:09 -0700

    Delete Medium/Friend Count in User Array directory

--- Medium/Friend Count in User Array/solution.py
+++
@@ -1,9 +0,0 @@
-def count_friends(user_ids):
-    cnt = {}
-    for row in user_ids:
-        for user in row:
-            if user in cnt.keys():
-                cnt[user]+=1
-            else:
-                cnt[user] = 1
-    return cnt
