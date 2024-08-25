656a9bf1f9cca4438155862ec208aa630d803ae2
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-08-25 13:35:49 -0700

    Delete Easy/Aggregate Listening Data/Salaries Difference directory

--- Easy/Aggregate Listening Data/Salaries Difference/sol.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  ABS((SELECT max(salary)
-   FROM db_employee emp
-   JOIN db_dept dept ON emp.department_id = dept.id
-   WHERE department = 'marketing') -
-  (SELECT max(salary)
-   FROM db_employee emp
-   JOIN db_dept dept ON emp.department_id = dept.id
-   WHERE department = 'engineering')) AS salary_difference
