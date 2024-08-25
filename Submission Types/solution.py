dddc71956647627e533caf454d3e9fb51402d1ca
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-08-25 13:09:06 -0700

    Delete Submission Types directory

--- Submission Types/solution.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-type_counts = loans.groupby('user_id')['type'].value_counts().unstack(fill_value=0).reset_index()
-
-filtered_users = type_counts[(type_counts['Refinance'] >= 1) & (type_counts['InSchool'] >= 1)]['user_id']
-
-result_df = pd.DataFrame(filtered_users)
-
-result_df
